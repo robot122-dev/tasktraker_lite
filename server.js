b324c8981a2499bb1d91f87bdd96aaa4cd3705cf
3. Vadim Bychkov <iam@vadim4ndreevich.ru>
2026-06-17 20:48:23 +0300

    fix: add root route and static files serving

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 // In-memory storage
 let tasks = [];
@@ -91,6 +92,11 @@ const swaggerOptions = {
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/api-docs.json', (req, res) => res.json(swaggerSpec));
+
+// Root route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 // Validation helpers
 const VALID_STATUSES = ['todo', 'in_progress', 'done'];
